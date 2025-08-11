4b1b0a9ef4850651e6207f5bb95a1374fe9df951
bidurPinnacle <bidur@pinnacleconsultingllc.com>
2025-08-11 23:16:48 +0530

    initial commit

---
+++ src/main/java/com/demoProject/bidur/demoProject/DemoProjApplication.java
@@ -0,0 +1,13 @@
+package com.demoProject.bidur.demoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoProject/bidur/demoProject/DemoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoProject.bidur.demoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
