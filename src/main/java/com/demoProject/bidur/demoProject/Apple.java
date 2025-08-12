2fbb48a174e4771664610312a6e46f18ef5bb888
bidurPinnacle <bidur@pinnacleconsultingllc.com>
2025-08-12 16:07:28 +0530

    chapter 3 done

--- src/main/java/com/demoProject/bidur/demoProject/Apple.java
+++ src/main/java/com/demoProject/bidur/demoProject/Apple.java
@@ -1,11 +1,22 @@
 package com.demoProject.bidur.demoProject;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 @Component
 //auto configured bean
 public class Apple {
     void getApples(){
         System.out.println("We are getting apples from autoconfigured bean");
+    }
+
+    @PostConstruct
+    void runAfterConstruction(){
+        System.out.println("This is from post Construction");
+    }
+    @PreDestroy
+    void runBeforeDestruction(){
+        System.out.println("This is from pre destroy");
     }
 
 }
